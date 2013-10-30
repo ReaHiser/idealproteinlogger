285870aea49d39df8c6d682132451dfb2db8f0f1
Bud Manz <admin@manzwebdesigns.com>
2013-10-30 12:39:18 -0400

    Initial commit to GitHub

--- app/routes.php
+++ app/routes.php
@@ -5,7 +5,7 @@ $app->match('/contact', 'default.controller:contactAction')->bind('contact')->me
 $app->match('/login', 'auth.controller:loginAction')->bind('login')->method('GET|POST');
 $app->get('/logout', 'auth.controller:logoutAction')->bind('logout');
 $app->match('/myaccount', 'default.controller:myAccountAction')->bind('my_account')->method('GET|POST');
-$app->get('/order_page', 'default.controller:orderpageAction')->bind('order_page');
+$app->get('/order_page', 'default.controller:orderPageAction')->bind('order_page')->method('GET|POST');
 
 $app->match('/admin/clients', 'admin.clients.controller:indexAction')->bind('admin_clients_index')->method('POST|GET')->before($mustAuthenticate)->before($isAdmin);
 $app->match('/admin/clients/create', 'admin.clients.controller:createAction')->bind('admin_clients_create')->method('POST|GET')->before($mustAuthenticate)->before($isAdmin);
